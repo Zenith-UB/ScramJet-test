0583a630c76978cb4f117d21af9a0a3fd6797152
Zenith <zenithjdozucwuxgwn@gmail.com>
2026-05-19 21:50:19 -0400

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,25 +1,10 @@
-"use strict";
-const stockSW = "./sw.js";
-
-/**
- * List of hostnames that are allowed to run serviceworkers on http://
- */
-const swAllowedHostnames = ["localhost", "127.0.0.1"];
-
-/**
- * Global util
- * Used in 404.html and index.html
- */
-async function registerSW() {
-	if (!navigator.serviceWorker) {
-		if (
-			location.protocol !== "https:" &&
-			!swAllowedHostnames.includes(location.hostname)
-		)
-			throw new Error("Service workers cannot be registered without https.");
-
-		throw new Error("Your browser doesn't support service workers.");
-	}
-
-	await navigator.serviceWorker.register(stockSW);
+if ('serviceWorker' in navigator) {
+  // Force the service worker to explicitly run inside your GitHub repository subfolder
+  navigator.serviceWorker.register('./sw.js', { 
+    scope: '/ScramJet-test/' 
+  }).then(() => {
+    console.log('Scramjet Service Worker successfully active on GitHub Pages!');
+  }).catch((err) => {
+    console.error('Service Worker registration failed:', err);
+  });
 }
